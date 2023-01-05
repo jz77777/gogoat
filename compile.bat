5bd9f8dd83065bd1b9ffa156b1d8281d81a46428
Jáchym Toušek <enumag@gmail.com>
2023-01-05 19:14:55 +0100

    Compilation

--- compile.bat
+++ compile.bat
@@ -1,10 +1,13 @@
 set GOARCH=amd64
 
 set GOOS=windows
-go build -o gogoat.exe gogoat.go
-
-set GOOS=darwin
-go build -o gogoat-mac gogoat.go
+go build -o bin/gogoat.exe gogoat.go
 
 set GOOS=linux
-go build -o gogoat-lin gogoat.go
+go build -o bin/gogoat-linux gogoat.go
+
+set GOOS=darwin
+go build -o bin/gogoat-mac gogoat.go
+
+set GOARCH=arm64
+go build -o bin/gogoat-mac-arm gogoat.go
