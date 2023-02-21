0a75b95e97cf8b3686dcb8174c2081c952f2d632
Jáchym Toušek <enumag@gmail.com>
2023-02-21 18:20:56 +0100

    Reduce size

--- compile.bat
+++ compile.bat
@@ -1,13 +1,13 @@
 set GOARCH=amd64
 
 set GOOS=windows
-go build -o bin/gogoat.exe gogoat.go
+go build -ldflags="-s -w" -o bin/gogoat.exe gogoat.go
 
 set GOOS=linux
-go build -o bin/gogoat-linux gogoat.go
+go build -ldflags="-s -w" -o bin/gogoat-linux gogoat.go
 
 set GOOS=darwin
-go build -o bin/gogoat-mac gogoat.go
+go build -ldflags="-s -w" -o bin/gogoat-mac gogoat.go
 
 set GOARCH=arm64
-go build -o bin/gogoat-mac-arm gogoat.go
+go build -ldflags="-s -w" -o bin/gogoat-mac-arm gogoat.go
